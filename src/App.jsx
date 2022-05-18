2944ff42e7dfc5578321bc4e2e27125a880a2855
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-18 10:32:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from  "react-router-dom"
+import Main from "./pages/Main"
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Title of Page</h1>
+
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+      </Routes>
+      
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/styled-components/Container.jsx
@@ -0,0 +1,10 @@
+import styled from "style-components"
+
+const Row = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+padding: 10px;
+`
+
+export default Row
